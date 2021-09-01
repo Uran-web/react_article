9770025adb48d11a65a81d4a78fb785aa3c50485
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-09-01 13:21:20 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.fcd1a40b213d928a738616861aeb4cca.js"
 );
 
 self.addEventListener('message', (event) => {
